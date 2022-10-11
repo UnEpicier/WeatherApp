1510146e2f5e327f957eda9e90d35613f5967708
Un Épicier <alexisvasseur96@gmail.com>
2022-10-11 15:57:16 +0200

    Init Project

---
+++ MainWindow.cs
@@ -0,0 +1,21 @@
+using Gtk;
+
+namespace Weather_App
+{
+    class MainWindow : Window
+    {
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
+        {
+            builder.Autoconnect(this);
+
+            DeleteEvent += Window_DeleteEvent;
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+        }
+    }
+}
