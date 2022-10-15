2d617315286dd6dfd3649f082ddfa925fa5eef50
RoxanneRlld <roxanne.roulland@ynov.com>
2022-10-15 13:38:30 +0200

    Init ForecastAPI + Glade UI

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,7 +1,11 @@
 using System;
+using System.IO;
 using Gtk;
 using UI = Gtk.Builder.ObjectAttribute;
 using Libs;
+using Newtonsoft.Json.Linq;
+using API;
+
 
 namespace Weather_App
 {
@@ -19,6 +23,74 @@ namespace Weather_App
          */
         [UI] private Button Search = null;
         [UI] private Button Settings = null;
+
+        /* 
+         ? Today Information Panel
+         */
+
+        [UI] private Image ImgToday = null;
+        [UI] private Label CityName = null;
+        [UI] private Label DateOfToday = null;
+        [UI] private Label TemperatureOfToday = null;
+        [UI] private Label FeelingsOfToday = null;
+        [UI] private Label DescWeatherToday = null;
+        [UI] private Label Latitude = null;
+        [UI] private Label Longitude = null;
+        [UI] private Label TempMin = null;
+        [UI] private Label TempMax = null;
+        [UI] private Label HumidityOfToday = null;
+        [UI] private Label WindOfToday = null;
+
+        /* 
+        ? DateBox 1
+         */
+        [UI] private Label DateBox1 = null;
+        [UI] private Label ImgWeather1 = null;
+        [UI] private Label Description1 = null;
+        [UI] private Label Time1 = null;
+        [UI] private Label Temperature1 = null;
+        [UI] private Label Humidity1 = null;
+
+        /* 
+        ? DateBox 2
+         */
+        [UI] private Label DateBox2 = null;
+        [UI] private Label ImgWeather2 = null;
+        [UI] private Label Description2 = null;
+        [UI] private Label Time2 = null;
+        [UI] private Label Temperature2 = null;
+        [UI] private Label Humidity2 = null;
+
+        /* 
+        ? DateBox 3
+         */
+        [UI] private Label DateBox3 = null;
+        [UI] private Label ImgWeather3 = null;
+        [UI] private Label Description3 = null;
+        [UI] private Label Time3 = null;
+        [UI] private Label Temperature3 = null;
+        [UI] private Label Humidity3 = null;
+
+        /* 
+        ? DateBox 4
+         */
+        [UI] private Label DateBox4 = null;
+        [UI] private Label ImgWeather4 = null;
+        [UI] private Label Description4 = null;
+        [UI] private Label Time4 = null;
+        [UI] private Label Temperature4 = null;
+        [UI] private Label Humidity4 = null;
+
+        /* 
+        ? DateBox 5
+         */
+        [UI] private Label DateBox5 = null;
+        [UI] private Label ImgWeather5 = null;
+        [UI] private Label Description5 = null;
+        [UI] private Label Time5 = null;
+        [UI] private Label Temperature5 = null;
+        [UI] private Label Humidity5 = null;
+
 
         public MainWindow() : this(new Builder("MainWindow.glade")) { }
 
@@ -74,6 +146,13 @@ Please check this problem then reopen the application.";
         private void QuitApplication_Clicked(object sender, EventArgs a)
         {
             Application.Quit();
+        }
+
+        private async void init_MainWindow(){
+            JObject options = JObject.Parse(File.ReadAllText("./options.json"));
+            FetchAPI api = new FetchAPI();
+            JObject data = await api.GetActualInfos("Bordeaux", "France", "fr");
+            data = FetchAPI.FormatData(data, "c");
         }
 
     }
